e61f06a8ea58a8429e7413b90bd5d412a47dee0c
Killol392 <killolrajgor0309@gmail.com>
2025-09-23 23:23:28 -0400

    Apostrophy and Quatations Changes for Deployement

--- app/Labs/Lab2/Float.tsx
+++ app/Labs/Lab2/Float.tsx
@@ -8,7 +8,7 @@ export default function Float() {
           src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
         />
         Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever
         since the 1500s, when an unknown printer took a galley of type and
         scrambled it to make a type specimen book. It has survived not only five
         centuries, but also the leap into electronic typesetting, remaining
@@ -27,13 +27,13 @@ export default function Float() {
         Latin words, consectetur, from a Lorem Ipsum passage, and going through
         the cites of the word in classical literature, discovered the
         undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33
-        of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by
+        of &quot;de Finibus Bonorum et Malorum&quot; (The Extremes of Good and Evil) by
         Cicero, written in 45 BC. This book is a treatise on the theory of
         ethics, very popular during the Renaissance. The first line of Lorem
-        Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section
+        Ipsum, &quot;Lorem ipsum dolor sit amet..&quot;, comes from a line in section
         1.10.32. The standard chunk of Lorem Ipsum used since the 1500s is
         reproduced below for those interested. Sections 1.10.32 and 1.10.33 from
-        "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their
+        &quot;de Finibus Bonorum et Malorum&quot; by Cicero are also reproduced in their
         exact original form, accompanied by English versions from the 1914
         translation by H. Rackham.
         <img
@@ -43,10 +43,10 @@ export default function Float() {
         It is a long established fact that a reader will be distracted by the
         readable content of a page when looking at its layout. The point of
         using Lorem Ipsum is that it has a more-or-less normal distribution of
-        letters, as opposed to using 'Content here, content here', making it
+        letters, as opposed to using &apos;Content here, content here&apos;, making it
         look like readable English. Many desktop publishing packages and web
         page editors now use Lorem Ipsum as their default model text, and a
-        search for 'lorem ipsum' will uncover many web sites still in their
+        search for &apos;lorem ipsum&apos; will uncover many web sites still in their
         infancy. Various versions have evolved over the years, sometimes by
         accident, sometimes on purpose (injected humour and the like).
         <img
@@ -55,8 +55,8 @@ export default function Float() {
         />
         There are many variations of passages of Lorem Ipsum available, but the
         majority have suffered alteration in some form, by injected humour, or
-        randomised words which don't look even slightly believable. If you are
-        going to use a passage of Lorem Ipsum, you need to be sure there isn't
+        randomised words which don&apos;t look even slightly believable. If you are
+        going to use a passage of Lorem Ipsum, you need to be sure there isn&apos;t
         anything embarrassing hidden in the middle of text. All the Lorem Ipsum
         generators on the Internet tend to repeat predefined chunks as
         necessary, making this the first true generator on the Internet. It uses
@@ -68,13 +68,13 @@ export default function Float() {
       </div><br></br>
       <div>
         <div className="wd-float-left wd-dimension-portrait wd-bg-color-yellow">
-          Yellow{" "}
+          Yellow
         </div>
         <div className="wd-float-left wd-dimension-portrait wd-bg-color-blue wd-fg-color-white">
-          Blue{" "}
+          Blue
         </div>
         <div className="wd-float-left wd-dimension-portrait wd-bg-color-red">
-          Red{" "}
+          Red
         </div>
         <img
           className="wd-float-right"
