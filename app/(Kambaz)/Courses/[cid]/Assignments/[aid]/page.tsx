c6b862c7b5648e82b06bb232f706def890c3e07a
Killol392 <killolrajgor0309@gmail.com>
2025-10-06 18:44:02 -0400

    a2 Complete

--- app/(Kambaz)/Courses/[cid]/Assignments/[aid]/page.tsx
+++ app/(Kambaz)/Courses/[cid]/Assignments/[aid]/page.tsx
@@ -99,7 +99,6 @@ The Kanbas application should include a link to navigate back to the landing pag
                 id="wd-website-url"
                 label="Website URL"
                 className="m-2"
-                defaultChecked
               />
               <FormCheck
                 type="checkbox"
