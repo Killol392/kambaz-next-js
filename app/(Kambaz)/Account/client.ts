bd4e85daa14c6695f23bfa3ee3be7f7d4f238929
Killol392 <killolrajgor0309@gmail.com>
2025-12-01 05:30:00 -0500

    Preview Environment Variable Created

--- app/(Kambaz)/Account/client.ts
+++ app/(Kambaz)/Account/client.ts
@@ -3,7 +3,10 @@ import axios from "axios";
 
 const axiosWithCredentials = axios.create({ withCredentials: true });
 
-export const HTTP_SERVER = process.env.NEXT_PUBLIC_HTTP_SERVER;
+// export const HTTP_SERVER = process.env.NEXT_PUBLIC_HTTP_SERVER;
+export const HTTP_SERVER =
+  process.env.NEXT_PUBLIC_HTTP_SERVER_PREVIEW ||
+  process.env.NEXT_PUBLIC_HTTP_SERVER;
 export const USERS_API = `${HTTP_SERVER}/api/users`;
 
 export const signin = async (credentials: any) => {
